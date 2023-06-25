31adcb8ef7b976d1421ec8c387abea43e9d432b3
sereypsv <valid_@tut.by>
2023-06-25 23:50:43 +0300

    Split App into App-stateful and AppLayout-stateless components

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { CreateButtons, Players } from './components';
 import styles from './app.module.css';
-import { ticTacToeStart } from './utils';
-import { TicTacToeWon } from './utils';
+
+import { CreateButtons, Players } from './components';
+import { ticTacToeStart, TicTacToeWon } from './utils';
 
 let playerN = 'O',
 	none = 'none',
 	message = '';
 
 export const App = () => {
-	let [ticTacToe, setTicTacToe] = useState(ticTacToeStart);
+	const [ticTacToe, setTicTacToe] = useState(ticTacToeStart);
 
 	const onClick = (item) => {
 		if (item.target.className === styles.field) {
@@ -32,22 +32,24 @@ export const App = () => {
 
 	const ticTacToeWon = TicTacToeWon(ticTacToe, playerN, none, message);
 
-	return (
-		<div className={styles.app} style={{ height: `${window.screen.height}px` }}>
-			<label className={styles.appLabel}>Tic Tac Toe</label>
-			<Players playerN={playerN} />
-			<div className={styles.field} onClick={onClick}>
-				<CreateButtons
-					buttons={ticTacToe}
-					ticTacToeBCG={TicTacToeWon(ticTacToe, playerN)}
-				/>
-			</div>
-			<button className={styles.buttonStart} onClick={onClick}>
-				Start Game!
-			</button>
-			<div className={styles.wonMessage} style={{ display: `${ticTacToeWon.none}` }}>
-				{ticTacToeWon.message}
-			</div>
-		</div>
-	);
+	return <AppLayout onClick={onClick} ticTacToe={ticTacToe} ticTacToeWon={ticTacToeWon} />;
 };
+
+const AppLayout = ({ onClick, ticTacToe, ticTacToeWon }) => (
+	<div className={styles.app} style={{ height: `${window.screen.height}px` }}>
+		<label className={styles.appLabel}>Tic Tac Toe</label>
+		<Players playerN={playerN} />
+		<div className={styles.field} onClick={onClick}>
+			<CreateButtons
+				buttons={ticTacToe}
+				ticTacToeBCG={TicTacToeWon(ticTacToe, playerN)}
+			/>
+		</div>
+		<button className={styles.buttonStart} onClick={onClick}>
+			Start Game!
+		</button>
+		<div className={styles.wonMessage} style={{ display: `${ticTacToeWon.none}` }}>
+			{ticTacToeWon.message}
+		</div>
+	</div>
+);
